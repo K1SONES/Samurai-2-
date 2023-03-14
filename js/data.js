dc286c45874f6facf0eac7dd56a511fff12ec57d
K1SONES <maplekaiser7@gmail.com>
2023-03-14 19:30:37 +0900

    4章 変数について 変数の宣言と値の代入

--- js/data.js
+++ js/data.js
@@ -7,3 +7,24 @@ console.log(45 % 8);
 console.log("45" + "18");
 console.log("今日の天気は" + "晴れ");
 console.log("今日の天気は" + "曇り");
+
+// 変数の宣言
+let userName;
+
+//値の代入
+userName = "侍太郎";
+
+//変数の宣言値の代入
+let UserNumber = 55;
+
+//コンソールへの出力
+console.log(userName);
+console.log(UserNumber);
+
+//値の再代入
+userName = "侍花子";
+UserNumber = "88";
+
+// コンソールへの出力
+console.log(userName);
+console.log(UserNumber);
