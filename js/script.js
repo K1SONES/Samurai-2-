682cbb5a730e20ae259b056d427abda03972b8df
K1SONES <maplekaiser7@gmail.com>
2023-03-22 20:24:31 +0900

    while文の書き方

--- js/script.js
+++ js/script.js
@@ -1,79 +1,94 @@
-// 定数の宣言・値の代入
-const shippingFee = 500;
+// // 定数の宣言・値の代入
+// const shippingFee = 500;
 
-// コンソールへの出力;
-console.log(shippingFee);
+// // コンソールへの出力;
+// console.log(shippingFee);
 
-const number = 50;
-// もし定数numが10より大きいならば、「定数numは10より大きいです」という文字列を出力する
-if (number > 10) {
-  console.log("定数numは10より大きいです");
-}
+// const number = 50;
+// // もし定数numが10より大きいならば、「定数numは10より大きいです」という文字列を出力する
+// if (number > 10) {
+//   console.log("定数numは10より大きいです");
+// }
 
-// 算術演算子を使った場合の戻り値を出力
-console.log(45 + 18);
+// // 算術演算子を使った場合の戻り値を出力
+// console.log(45 + 18);
 
-// 比較演算子を使った場合の戻り値を出力
-console.log(45 > 18);
+// // 比較演算子を使った場合の戻り値を出力
+// console.log(45 > 18);
 
-// 等価演算子を使った場合の戻り値を出力
-console.log("5" == 5);
+// // 等価演算子を使った場合の戻り値を出力
+// console.log("5" == 5);
 
-// 厳密等価演算子を使った場合の戻り値を出力
-console.log("5" === 5);
+// // 厳密等価演算子を使った場合の戻り値を出力
+// console.log("5" === 5);
 
-// 変数numに0～4までのランダムな整数を代入する
-let num = Math.floor(Math.random() * 5);
+// // 変数numに0～4までのランダムな整数を代入する
+// let num = Math.floor(Math.random() * 5);
 
-// 変数numの値を出力する
-console.log(num);
+// // 変数numの値を出力する
+// console.log(num);
 
-// 変数numの値が4であれば「大当たりです」という文字列を出力する
-if (num === 4) {
-  console.log("大当たりです");
-}
-//変数numの値が3であれば「あたりです」という文字列を出力する
-else if (num === 3) {
-  console.log("あたりです");
-}
-// それ以外の時は、「はずれです」という文字列を出力する
-else {
-  console.log("はずれです");
-}
+// // 変数numの値が4であれば「大当たりです」という文字列を出力する
+// if (num === 4) {
+//   console.log("大当たりです");
+// }
+// //変数numの値が3であれば「あたりです」という文字列を出力する
+// else if (num === 3) {
+//   console.log("あたりです");
+// }
+// // それ以外の時は、「はずれです」という文字列を出力する
+// else {
+//   console.log("はずれです");
+// }
 
-// //変数numに0~2までのランダムな整数を代入する
-// let num = Math.floor(Math.random() * 3);
+// // //変数numに0~2までのランダムな整数を代入する
+// // let num = Math.floor(Math.random() * 3);
 
-// //変数numの値によって出力する文字列を切り替える
+// // //変数numの値によって出力する文字列を切り替える
+// // switch (num) {
+// //   case 0:
+// //     console.log("小吉です");
+// //     break;
+// //   case 1:
+// //     console.log("中吉です");
+// //     break;
+// //   case 2:
+// //     console.log("大吉です");
+// //     break;
+// // }
+
+// num = Math.floor(Math.random() * 5);
+
+// //変数numの値を出力する
+// console.log(num);
+
+// //変数numの値によって、出力する文字列を切り替える
 // switch (num) {
-//   case 0:
+//   case 2:
 //     console.log("小吉です");
 //     break;
-//   case 1:
+//   case 3:
 //     console.log("中吉です");
 //     break;
-//   case 2:
+//   case 4:
 //     console.log("大吉です");
+//     break;
+//   default:
+//     console.log("末吉です");
 //     break;
 // }
 
-num = Math.floor(Math.random() * 5);
+// 変数numに0~4までのランダムな整数を代入する
+let num = Math.floor(Math.random() * 5);
 
-//変数numの値を出力する
-console.log(num);
+// 変数numの最初の値を出力する
+console.log("最初の値は" + num + "です");
 
-//変数numの値によって、出力する文字列を切り替える
-switch (num) {
-  case 2:
-    console.log("小吉です");
-    break;
-  case 3:
-    console.log("中吉です");
-    break;
-  case 4:
-    console.log("大吉です");
-    break;
-  default:
-    console.log("末吉です");
-    break;
+//変数numの値が0以外である間、変数numの値を出力し続ける
+while (num !== 0) {
+  //変数numに０～４までのランダムな整数を再代入する
+  num = Math.floor(Math.random() * 5);
+
+  //次の条件式で比較される変数numの現在の値を出力する
+  console.log("現在の値は" + num + "です");
 }
