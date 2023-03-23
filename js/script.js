e597e1b63ee55ee6bdb6d97c49a3143fba1ad5a4
K1SONES <maplekaiser7@gmail.com>
2023-03-23 21:10:38 +0900

    9章.配列

--- js/script.js
+++ js/script.js
@@ -97,3 +97,22 @@ while (num !== 0) {
 for (let i = 1; i <= 10; i++) {
   console.log(i);
 }
+
+//[配列]//
+
+//配列の宣言と値の代入を行う
+const userNames = ["侍太郎", "侍一郎", "侍二郎", "侍三郎", "侍四郎"];
+
+//配列の値の確認
+console.log(userNames);
+
+//二番目の要素を更新する
+userNames[1] = "侍花子";
+
+// 6番目に要素を追加する
+userNames[5] = "侍五郎";
+
+//配列の値を出力する
+console.log(userNames);
+
+console.log(userNames[2]);
