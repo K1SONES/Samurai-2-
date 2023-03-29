721c7071cff72b86a76f7d72b5de6cab6cfd6af5
K1SONES <maplekaiser7@gmail.com>
2023-03-29 20:19:41 +0900

    引数残りとスコープについて

--- js/script.js
+++ js/script.js
@@ -170,3 +170,23 @@ const addTwoArguments = (price, shippingFee) => {
 
 // 関数を呼び出し、引数として購入金額と送料を渡す
 addTwoArguments(1200, 500);
+
+//与えられた引数numを2倍にし、その値を戻り値として返す関数を定義
+const double = (num) => {
+  return num * 2;
+};
+
+//// 関数の戻り値を出力する
+console.log(double(30));
+
+// 関数の中で定数を宣言し、関数の中で定数を使う
+const useVariable = () => {
+  const userName = "侍太郎";
+  console.log(userName);
+};
+
+// 関数を呼び出す
+useVariable();
+
+// 関数の中で宣言した定数を関数の外で使う
+console.log(userName);
