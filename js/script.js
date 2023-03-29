a4239652e612006b22ccd30b1a086d87cda6b589
K1SONES <maplekaiser7@gmail.com>
2023-03-29 20:55:06 +0900

    メソッド

--- js/script.js
+++ js/script.js
@@ -200,6 +200,10 @@ class Product {
     this.price = price;
     this.category = category;
   }
+  //　メソッドを定義する
+  describe() {
+    console.log("この商品名は" + this.name + "です。");
+  }
 }
 
 // インスタンス化する
@@ -209,3 +213,21 @@ const coffee = new Product("コーヒー", 1500, "飲料");
 // インスタンス（オブジェクト）の値を出力する
 console.log(shampoo);
 console.log(coffee);
+
++(
+  // メソッドを呼び出す（実行する）
+  (+shampoo.describe())
+);
+
+// 通常のオブジェクトにメソッドを定義する
+const user = {
+  name: "侍太郎",
+  age: 36,
+  gender: "男性",
+  greet: () => {
+    console.log("よろしくお願いします！");
+  },
+};
+
+// メソッドを呼び出す（実行する）
+user.greet();
