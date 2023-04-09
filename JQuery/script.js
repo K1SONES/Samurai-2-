51795156dea4460f83d1a58be4ab6e289904d0bd
K1SONES <maplekaiser7@gmail.com>
2023-04-09 19:45:48 +0900

    onメソッド　と要素の抽出？

--- JQuery/script.js
+++ JQuery/script.js
@@ -4,3 +4,18 @@ $(function () {
   console.log(title);
   console.log(list);
 });
+// class属性値の先頭が「sample」の要素だけを抽出する
+const text = $('li[class^="sample"]').text();
+console.log(text);
+
+$(function () {
+  //ボタンがクリックされたら
+  $("button").on("click", function () {
+    console.log("ボタンがクリックされました");
+    console.log("ボタンがクリックされました");
+    console.log("ボタンがクリックされました");
+    setTimeout(() => {
+      console.log("ボタンがクリックされました!");
+    }, 1000);
+  });
+});
