336c32661297364d0af3f0895eb0bcfd8570ed66
K1SONES <maplekaiser7@gmail.com>
2023-04-19 21:43:32 +0900

    リンクをホバー時に半透明にする

--- JQ/script.js
+++ JQ/script.js
@@ -1,3 +1,13 @@
+// リンクのホバー時に不透明度をアニメーションで変更する
+$("a").hover(
+  function () {
+    $(this).animate({ opacity: 0.5 }, 500);
+  },
+  function () {
+    $(this).animate({ opacity: 1.0 }, 300);
+  }
+);
+
 // カルーセル
 $(".carousel").slick({
   autoplay: true,
