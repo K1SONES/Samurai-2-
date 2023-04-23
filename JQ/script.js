4b7b2e1a87afb1918f740d7bac2c409c1f5bbdc4
K1SONES <maplekaiser7@gmail.com>
2023-04-23 22:33:50 +0900

    top-50tuika

--- JQ/script.js
+++ JQ/script.js
@@ -45,7 +45,8 @@ $(function () {
     () => {
       let href = $(this).attr("href");
       let target = $(href == "#" || href == "" ? "html" : href);
-      let position = target.offset().top;
+      console.log();
+      let position = target.offset().top - 50;
       $("body,html").animate({ scrollTop: position }, 1000, "swing");
       return false;
     };
