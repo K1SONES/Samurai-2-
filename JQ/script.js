9859a8c1c5b3a77add03656a7d38b071aeda1005
K1SONES <maplekaiser7@gmail.com>
2023-04-25 23:02:33 +0900

    スムーススクロール完成

--- JQ/script.js
+++ JQ/script.js
@@ -41,13 +41,11 @@ $(function () {
     }
   });
   //リンクにスクロール
-  $("a[href^='#']").click,
-    () => {
-      let href = $(this).attr("href");
-      let target = $(href == "#" || href == "" ? "html" : href);
-      console.log();
-      let position = target.offset().top - 50;
-      $("body,html").animate({ scrollTop: position }, 1000, "swing");
-      return false;
-    };
+  $("a[href^='#']").click(function () {
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? "html" : href);
+    let position = target.offset().top - 50;
+    $("body,html").animate({ scrollTop: position }, 1000, "swing");
+    return false;
+  });
 });
