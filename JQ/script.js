d0b85c7baa2856a622c200777ecb195302158ac3
K1SONES <maplekaiser7@gmail.com>
2023-05-02 20:32:11 +0900

    modal

--- JQ/script.js
+++ JQ/script.js
@@ -48,4 +48,17 @@ $(function () {
     $("body,html").animate({ scrollTop: position }, 1000, "swing");
     return false;
   });
+  $(".item a").click(function () {
+    var imgSrc = $(this).children().attr("src");
+    $(".bigimg").children().attr("src", imgSrc);
+    $(".modal").fadeIn();
+    $("body,html").css("overflow-y", "hidden");
+    return false;
+  });
+
+  $(".close-btn").click(function () {
+    $(".modal").fadeOut();
+    $("body,html").css("overflow-y", "visible");
+    return false;
+  });
 });
